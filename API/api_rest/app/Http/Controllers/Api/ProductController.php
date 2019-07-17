c61dfeac5516bb75e85e61cd6fa984a4bb8e049a
Mateus Vinicios Sorgatto Giroletti <mateusgiroletti97@gmail.com>
2019-07-17 13:33:40 -0300

    criando migrates, factory e seedes do products

---
+++ API/api_rest/app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Product;
+
+class ProductController extends Controller
+{
+    public function index(){
+        return Product::all();
+    }
+}
