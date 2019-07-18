be552d2e16303930a3a4223177aed2b072e843f3
Mateus Vinicios Sorgatto Giroletti <mateusgiroletti97@gmail.com>
2019-07-18 00:08:35 -0300

    criando crud do product

--- API/api_rest/app/Http/Controllers/Api/ProductController.php
+++ API/api_rest/app/Http/Controllers/Api/ProductController.php
@@ -5,10 +5,69 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Product;
+use App\API\ApiError;
 
 class ProductController extends Controller
 {
-    public function index(){
-        return Product::all();
+    private $product;
+
+    public function __construct(Product $product)
+    {
+        $this->product = $product;
+    }
+    public function index()
+    {
+        $data = ['data' => $this->product->paginate(10)];
+        return response()->json($data);
+    }
+    public function show(Product $id)
+    {
+        return $id;
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            $productData = $request->all();
+            $this->product->create($productData);
+
+            $return = ['data' => ['msg' => 'Produto criado com sucesso!']];
+            return response()->json($return, 201);
+        } catch (\Exception $e) {
+            if (config(('app.debug'))) {
+                return response()->json(ApiError::errorMessage($e->getMessage(), 1010));
+            }
+            return response()->json(ApiError::errorMessage('Houve um erro ao utilizar a operação cadastrar', 1010));
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        try {
+            $productData = $request->all();
+            $product = $this->product->find($id);
+            $product->update($productData);
+
+            $return = ['data' => ['msg' => 'Produto atualizado com sucesso!']];
+            return response()->json($return, 201);
+        } catch (\Exception $e) {
+            if (config(('app.debug'))) {
+                return response()->json(ApiError::errorMessage($e->getMessage(), 1011));
+            }
+            return response()->json(ApiError::errorMessage('Houve um erro ao utilizar a operação atualizar', 1011));
+        }
+    }
+
+    public function delete(Product $id)
+    {
+        try {
+            $id->delete();
+            return response()->json(['data' => ['msg' => 'Produto: ' . $id->name . ' removido com sucesso!']], 200);
+        } catch (\Throwable $th) {
+            if (config(('app.debug'))) {
+                return response()->json(ApiError::errorMessage($e->getMessage(), 1012));
+            }
+            return response()->json(ApiError::errorMessage('Houve um erro ao utilizar a operação de deletar', 1012));
+        }
     }
 }
--- API/api_rest/routes/api.php
+++ API/api_rest/routes/api.php
@@ -18,5 +18,16 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::namespace('API')->group(function(){
-    Route::get('/products', 'ProductController@index')->name('products');
+    Route::prefix('products')->group(function(){
+        Route::get('/', 'ProductController@index')->name('index_products');
+        Route::get('/{id}','ProductController@show')->name('single_products');
+
+        Route::post('/', 'ProductController@store')->name('store_products');
+
+        Route::put('/{id}', 'ProductController@update')->name('update_products');
+
+        Route::delete('/{id}', 'ProductController@delete')->name('delete_products');
+
+
+    });
 });
